bce5fb149d105f84dd277b6e584e5a5b59d747ab
Good Lookin <r_a_msmlm@outlook.com>
2022-05-24 14:19:00 +0300

    UPDATED ADD MEMBER + ADD BOOK

--- src/components/AddBookModal.js
+++ src/components/AddBookModal.js
@@ -1,115 +1,128 @@
-import { Modal, Button, Form } from 'react-bootstrap';
-import React, { useState } from 'react';
-import bookStore from '../stores/bookStore';
+import { Modal, Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import bookStore from "../stores/bookStore";
 
 function AddBookModal() {
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(false);
+  const [addBook, setAddBook] = useState({
+    // "_id": '',
+    author: "",
+    title: "",
+    genres: ["Fantasy"],
+    available: true,
+    // "borrowedBy": [],
+    // "slug": '',
+    image: "",
+  });
 
-    const [show, setShow] = useState(false);
-    const [addBook, setAddBook] = useState({
-        "_id": '',
-        "author": '',
-        "title": '',
-        "genres": ["Fantasy"],
-        "available": true,
-        "borrowedBy": [],
-        "slug": '',
-        "image": ''
-    });
+  const handleClose = () => setShow(false);
 
-    const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const handleShow = () => setShow(true);
+  const handleChange = (event) => {
+    setAddBook({ ...addBook, [event.target.name]: event.target.value });
+  };
 
-    const handleChange = (event) => {
-        setAddBook({ ...addBook, [event.target.name]: event.target.value, });
+  const handleGenre = (event) => {
+    const bookarr = addBook;
+    if (count === 0) {
+      bookarr.genres.pop();
+      setCount(count + 1);
     }
+    bookarr.genres.push(event.target.value);
 
-    // const handleGenre = (event) => {
-    //     setAddBook({... addBook, [event.target.name]: push(event.target.value), });
-    // }
+    setAddBook(bookarr);
+    // setAddBook({
+    //   ...addBook,
+    //   [event.target.name]: addBook.genres.push(event.target.value),
+    // });
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        bookStore.createBook(addBook);
-        handleClose();
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    bookStore.createBook(addBook);
+    handleClose();
+  };
 
   return (
     <div>
       <Button variant="success" onClick={handleShow}>
-                Add New Book
-        </Button>
-      
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Add A Book</Modal.Title>
-            </Modal.Header>
+        Add New Book
+      </Button>
 
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Author Name</Form.Label>
-                        <Form.Control
-                            onChange={handleChange}
-                            type="text"
-                            placeholder="Author Name"
-                            autoFocus
-                            name="author"
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Book Title</Form.Label>
-                        <Form.Control
-                            onChange={handleChange}
-                            type="text"
-                            placeholder="Book Title"
-                            autoFocus
-                            name="title"
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Choose Genre</Form.Label>
-                        <Form.Select aria-label="Floating label select example" name="genres" onChange={handleChange}>
-                            <option selected>Select A Genre</option>
-                            <option value="Action">Action</option>
-                            <option value="Fantasy">Fantasy</option>
-                            <option value="Sci-Fi">Sci-Fi</option>
-                            <option value="Romance">Romance</option>
-                            <option value="Fiction">Fiction</option>
-                            <option value="Self-Help">Self-Help</option>
-                            <option value="Thriller">Thriller</option>
-                            <option value="Suspense">Suspense</option>
-                            <option value="Biography">Biography</option>
-                            <option value="Business">Business</option>
-                            <option value="Entrepreneurship">Entrepreneurship</option>
-                            <option value="Crime">Crime</option>
-                            <option value="Mystery">Mystery</option>
-                         </Form.Select>
-                    </Form.Group>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add A Book</Modal.Title>
+        </Modal.Header>
 
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Book Image URL</Form.Label>
-                        <Form.Control
-                            onChange={handleChange}
-                            type="text"
-                            placeholder="Book Image URL"
-                            autoFocus
-                            name="image"
-                        />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Add Book
-                    </Button>
-                </Form>
-            </Modal.Body>
-        </Modal>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Author Name</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="Author Name"
+                autoFocus
+                name="author"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Book Title</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="Book Title"
+                autoFocus
+                name="title"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Choose Genre</Form.Label>
+              <Form.Select
+                aria-label="Floating label select example"
+                name="genres"
+                onChange={handleGenre}
+              >
+                <option selected>Select A Genre</option>
+                <option value="Action">Action</option>
+                <option value="Fantasy">Fantasy</option>
+                <option value="Sci-Fi">Sci-Fi</option>
+                <option value="Romance">Romance</option>
+                <option value="Fiction">Fiction</option>
+                <option value="Self-Help">Self-Help</option>
+                <option value="Thriller">Thriller</option>
+                <option value="Suspense">Suspense</option>
+                <option value="Biography">Biography</option>
+                <option value="Business">Business</option>
+                <option value="Entrepreneurship">Entrepreneurship</option>
+                <option value="Crime">Crime</option>
+                <option value="Mystery">Mystery</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Book Image URL</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="Book Image URL"
+                autoFocus
+                name="image"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Add Book
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
 
 export default AddBookModal;
-
-
 
 // <Form.Control
 // onChange={handleChange}
@@ -117,11 +130,11 @@ export default AddBookModal;
 // placeholder="Book Image URL"
 // autoFocus
 // name="genre"
-// {   <select 
+// {   <select
 //         className='genre-specs'
 //         aria-label="Default select example"
 //     >
 //         <option value="">All</option>
-        
+
 //     </select>}
 // />
--- src/stores/bookStore.js
+++ src/stores/bookStore.js
@@ -2,73 +2,78 @@ import { makeAutoObservable } from "mobx";
 import axios from "axios";
 
 class BookStore {
-    
-    constructor() {
-        makeAutoObservable(this);
-      }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    books = [];
+  books = [];
 
-    fetchBook = async () => {
-        try {
-            const response = await axios.get("https://library-borrow-system.herokuapp.com/api/books");
-            this.books = response.data;
-            console.log("Books: "+JSON.stringify(this.books));
-        } catch (error) {
-            console.error(error);
-        }
+  fetchBook = async () => {
+    try {
+      const response = await axios.get(
+        "https://library-borrow-system.herokuapp.com/api/books"
+      );
+      this.books = response.data;
+      // console.log("Books: "+JSON.stringify(this.books));
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    generateID = (length) => {
-        const characters ='abcdefghijklmnopqrstuvwxyz';
-        let result = '';
-        const charactersLength = characters.length;
-        for ( let i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
+  //   generateID = (length) => {
+  //     const characters = "abcdefghijklmnopqrstuvwxyz";
+  //     let result = "";
+  //     const charactersLength = characters.length;
+  //     for (let i = 0; i < length; i++) {
+  //       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  //     }
+  //     return result;
+  //   };
+
+  createBook = async (book) => {
+    // const s1 = book.title.toLowerCase();
+    // const s2 = s1.replaceAll(":", "");
+    // const result = s2.replaceAll(" ", "-");
+
+    // book._id = "62853a01d8ec5d3d4a29fa9" + this.generateID(1);
+    // console.log("id: " + book._id);
+    // book.slug = result;
+    // console.log("slug: " + book.slug);
+
+    // console.log("Book: " + JSON.stringify(book));
+
+    try {
+      const response = await axios.post(
+        "https://library-borrow-system.herokuapp.com/api/books",
+        book
+      );
+      this.books.push(response.data);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    createBook = async (book) => {
-        const s1 = book.title.toLowerCase();
-        const s2 = s1.replaceAll(':', '');
-        const result = s2.replaceAll(' ', '-');
+  returnBook = async (bookId, memberId) => {
+    console.log("bookId: " + bookId);
+    console.log("memberId: " + memberId);
+    const book = this.books.find((b) => b._id === bookId);
+    book.available = true;
+    // const removedbook = book.borrowedBy.filter((bID => bID !== memberID));
+    // book.borrowedBy = removedbook;
 
-        book._id = "62853a01d8ec5d3d4a29fa9"+this.generateID(1);
-        console.log("id: "+book._id);
-        book.slug = result;
-        console.log("slug: "+book.slug);
-
-        console.log("Book: "+JSON.stringify(book));
-
-        try{
-          const response = await axios.post("https://library-borrow-system.herokuapp.com/api/books", book);
-          this.books.push(response.data);
-        }catch(error){
-          console.error(error);
-        }
-      };
-
-    returnBook = async (bookId, memberId) => {
-        console.log("bookId: "+bookId);
-        console.log("memberId: "+memberId);
-         const book = this.books.find(b => b._id === bookId);
-         book.available = true;
-        // const removedbook = book.borrowedBy.filter((bID => bID !== memberID));
-        // book.borrowedBy = removedbook;
-
-        try {
-            await axios.put(`https://library-borrow-system.herokuapp.com/api/books/${bookId}/return/${memberId}`);
-        } catch (error) {
-            console.error(error);
-        }
+    try {
+      await axios.put(
+        `https://library-borrow-system.herokuapp.com/api/books/${bookId}/return/${memberId}`
+      );
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    bookborrowed = (bookId, memberId) => {
-        const book = this.books.find((b) => b._id === bookId);
-        book.borrowedBy.push(memberId);
-    }
-
+  bookborrowed = (bookId, memberId) => {
+    const book = this.books.find((b) => b._id === bookId);
+    book.borrowedBy.push(memberId);
+  };
 }
 
 const bookStore = new BookStore();
